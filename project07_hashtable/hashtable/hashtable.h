8c011d895d1289b5af2c49ca09b1454ddbb06c11
Casey Nord <nordcw@gmail.com>
2018-05-21 09:25:54 -0700

    Roughed out project07_hashtable

--- project07_hashtable/hashtable/hashtable.h
+++ project07_hashtable/hashtable/hashtable.h
@@ -7,25 +7,134 @@
 // Program Description: Hashtable
 // Complexity: 
 //*****************************************************************
+
+/*
+
+This hashtable takes a value and determines an array index based on a very
+simple hashing algorithm...
+
+It may be obvious but collisions will instantly put us in situations where
+we could lose track of data.
+
+An example of this would be a collision where an item would be moved to a
+different index.
+
+If the previous data (i.e. parent data to data added that experienced collision)
+is deleted, then the hash will not be able to locate the collision.
+
+Though this is undesirable, it illustrates an important problem with hashing
+and gives insight into the complexity of designing a good hash.
+
+Because of this I have implemented a (very poor) way to handle collisions
+that causes the data to pile up after the value's index that it collides
+with.
+
+Again, this illustrates an important problem with hashing and gives insight
+into the complexity of designing a good hash.
+
+*/
+
 #include <cstdlib>
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
+#define MAX 10
 
 template <class T>
 class Hashtable
 {
     private:
+        T myTable[MAX];
+        T collisionTable[MAX];
+
+        int collisionIndex; 
 
     public:
         Hashtable();
+        int hash(T data);
+        void addItem(T newValue);
+        void deleteItem(T deleteData);
+        void printList();
 };
 
 
 template <class T>
 Hashtable<T>::Hashtable()
 {
+    collisionIndex = 0;
 
+    for (int i = 0; i < MAX; i++)
+    {
+        myTable[i] = NULL;
+    }
 }
+
+template <class T>
+int Hashtable<T>::hash(T data)
+{
+    if (data % 10 == 0)
+    {
+        data++;
+    }
+
+    data = data * 42 % MAX;
+
+    return data; 
+}
+
+template <class T>
+void Hashtable<T>::addItem(T newValue)
+{
+    int index = hash(newValue);
+
+    if (myTable[index] == NULL)
+    {
+        myTable[index] = newValue;
+    }
+    else
+    {
+        while(myTable[index] != NULL)
+        {
+            if (myTable[index] < MAX)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+        myTable[index] = newValue;
+        collisionTable[collisionIndex] = newValue;
+        collisionIndex++;
+
+    }
+}
+
+template <class T>
+void Hashtable<T>::deleteItem(T deleteData)
+{
+    int index = hash(deleteData);
+
+    if (myTable[index] == deleteData)
+    {
+        myTable[index] = NULL;
+        cout << "The value " << deleteData << " has been removed from the table" << endl << endl;
+    }
+    else
+    {
+        cout << "The value " << deleteData << " wasn't found in the table" << endl << endl;
+    }
+}
+
+template <class T>
+void Hashtable<T>::printList() // Total (worst case): O(2n)
+{
+    for (int i = 0; i < MAX; i++) // O(n)
+    {
+        cout << myTable[i] << endl; // O(n)
+    }
+}
+
--- project07_hashtable/hashtable/testHashtable.cpp
+++ project07_hashtable/hashtable/testHashtable.cpp
@@ -18,7 +18,20 @@ using std::endl;
 
 int main(int argc, char** argv)
 {
+    Hashtable<int> myHash;
 
+    myHash.addItem(10);
+    myHash.addItem(67);
+    myHash.addItem(42);
+    // HANGS HERE myHash.addItem(36);
+    //myHash.addItem(13);
+    //myHash.addItem(77);
+    //myHash.addItem(44);
+    //myHash.addItem(41);
+    //myHash.addItem(89);
+    //myHash.addItem(127);
+
+    myHash.printList();
 
     return 0;
 }
