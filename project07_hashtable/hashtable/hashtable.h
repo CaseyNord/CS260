8b51bef8566c089de11080dc7adf1f6874e3dc84
Casey Nord <nordcw@gmail.com>
2018-05-21 08:14:37 -0700

    Added files/folders for project07_hashtables

---
+++ project07_hashtable/hashtable/hashtable.h
@@ -0,0 +1,31 @@
+//*****************************************************************
+// Name: Casey Nord
+// Class: CS260 Spring 2018
+// Class Time: Mon/Wed/Fri 8:00am
+// Date: May 21, 2018
+// Project #7
+// Program Description: Hashtable
+// Complexity: 
+//*****************************************************************
+#include <cstdlib>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+template <class T>
+class Hashtable
+{
+    private:
+
+    public:
+        Hashtable();
+};
+
+
+template <class T>
+Hashtable<T>::Hashtable()
+{
+
+}
---
+++ project07_hashtable/hashtable/testHashtable.cpp
@@ -0,0 +1,24 @@
+//*****************************************************************
+// Name: Casey Nord
+// Class: CS260 Spring 2018
+// Class Time: Mon/Wed/Fri 8:00am
+// Date: May 21, 2018
+// Project #7
+// Program Description: Hashtable
+// Complexity: 
+//*****************************************************************
+#include <cstdlib>
+#include <iostream>
+
+#include "Hashtable.h"
+
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char** argv)
+{
+
+
+    return 0;
+}
