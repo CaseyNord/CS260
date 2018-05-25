db61d1599698b6f602db571efddae35d4da5a9d7
Casey Nord <nordcw@gmail.com>
2018-05-25 09:06:57 -0700

    Finished project07_hashtable

--- project07_hashtable/hashtable/hashtable.h
+++ project07_hashtable/hashtable/hashtable.h
@@ -13,24 +13,50 @@
 This hashtable takes a value and determines an array index based on a very
 simple hashing algorithm...
 
-It may be obvious but collisions will instantly put us in situations where
-we could lose track of data.
+Important notes:
+    With a small table (an array of 10 indexes), a list of 1500 items will
+    cause so many collisions that the table will never actually grow.  That is
+    to say that the hashing algorithm with a list of 1500 random words never
+    produces a range of indexes that would make it hit the rehash threshold.
 
-An example of this would be a collision where an item would be moved to a
-different index.
+    With an array capacity of 100 indexes, a list of 10000 items only produces
+    16 unique hash indexes.
 
-If the previous data (i.e. parent data to data added that experienced collision)
-is deleted, then the hash will not be able to locate the collision.
+    With an array capacity of 1000 indexes, a list of 10000 items only produces
+    about 355 unique hash indexes
 
-Though this is undesirable, it illustrates an important problem with hashing
-and gives insight into the complexity of designing a good hash.
+Although I initially wanted to experiment with using something like a linked list
+to handle collisions, I opted not to.  The idea would be to create a reference to
+a linked list and place it at the index value where a collision occured, and then
+populate the list with all of the data that collides at that index.  We could then
+search the linked list whenever we hit that index.
 
-Because of this I have implemented a (very poor) way to handle collisions
-that causes the data to pile up after the value's index that it collides
-with.
+These kinds of solutions seem common with hashtables.  You could also use arrays,
+trees, or any other structure to handle a collision.  I think this is what is being
+referred to when people use the term 'chaining'.  One idea I thought about was using
+another hashtable!  Maybe one that was designed to create less collisions out of the
+data that collides at a specific index.
 
-Again, this illustrates an important problem with hashing and gives insight
-into the complexity of designing a good hash.
+I opted not to because in my tests I found that I could run lists of 10000 items with
+an array of size 1000, and I only got about 355 unique hashes.  As a result I never
+resized my table.  Instead I opted to handle collisions very simply (poorly) by just
+iterating through the array until an empty spot was reached.  Doing this allows me to
+test my resizing functions, and although it's probably slow, I'd think it still averages 
+faster than linear (at least with my test data) because when a search is made we still 
+probably get closer to the value by initially hashing before we start iterating through
+the list linearly.
+
+Honestly, hashing seems to be a HUGE topic with endless potential exploration and when
+I think about ways to solve some of these problems I just start coming up with cascades
+of things I would like to try.  At the end of the day, before I go down the rabbit hole
+of trying to find what I can come with that might be OKAY, I would prefer to read some
+material on the approaches that have already been developed so I can form a foundation
+upon which to establish my own ideas, that would hopefully allow me to come up with some 
+solutions that could actuablly be viable, instead of just trying to hack together
+whatever 'creative' ideas I might have.
+
+So, as a result, I am submitting this high collision hash table that handles collisions
+with linear searches!
 
 */
 
@@ -40,21 +66,26 @@ into the complexity of designing a good hash.
 using std::cout;
 using std::endl;
 
-#define MAX 10
 
 template <class T>
 class Hashtable
 {
     private:
-        T myTable[MAX];
-        T collisionTable[MAX];
-
-        int collisionIndex; 
+        float loadFactor;
+        int spaceUsed;
+        int initialCapacity;
+        int capacity;
+        int rehashThreshold;
+        int hashMask;
+        
+        T *hashTable;
 
     public:
         Hashtable();
         int hash(T data);
-        void addItem(T newValue);
+        void clearTable(T table[]);
+        void growTable(T oldHashTable[], T addData);
+        void addItem(T newData);
         void deleteItem(T deleteData);
         void printList();
 };
@@ -63,53 +94,138 @@ class Hashtable
 template <class T>
 Hashtable<T>::Hashtable()
 {
-    collisionIndex = 0;
+    loadFactor = 0.75f;
+    spaceUsed = 0;
+    initialCapacity = 10;
+    capacity = initialCapacity;
+    rehashThreshold = (int)(capacity * loadFactor);
+    hashMask = capacity - 1;
 
-    for (int i = 0; i < MAX; i++)
+    hashTable = new T[capacity];
+
+    // When the hashtable is created we want to NULL out the values so we can use them utilize the value for future operations.
+    // And it's good practice...
+    clearTable(hashTable);
+
+}
+
+template <class T>
+void Hashtable<T>::clearTable(T table[])
+{
+    for (int i = 0; i < capacity; i++)
     {
-        myTable[i] = NULL;
+        table[i] = "";
     }
+}
+
+template <class T>
+void Hashtable<T>::growTable(T oldHashTable[], T addData)
+{
+    cout << "Threshold reached, growing table..." << endl;
+
+    cout << "Creating temporary table to transfer data..." << endl;
+
+    T *tempTable = new T[spaceUsed];
+    int tempCounter = 0;
+    for (int i = 0; i < capacity; i++)
+    {
+        if (oldHashTable[i] != "")
+        {
+            tempTable[tempCounter] = oldHashTable[i];
+            tempCounter++;
+        }
+    }
+
+    cout << "Temporary table created, deleting old table..." << endl;
+
+    delete[] oldHashTable;
+
+    cout << "Old table deleted, initializing new values..." << endl;
+
+    capacity = capacity * 2;
+    rehashThreshold = (int)(capacity * loadFactor);
+    hashMask = capacity - 1;
+
+    cout << "Values initialized, creating new table..." << endl;
+
+    hashTable = new T[capacity];
+
+    cout << "New table created, initializing indexes..." << endl;
+
+    // When the hashtable is created we want to NULL out the values so we can use them utilize the value for future operations.
+    // And it's good practice...
+    clearTable(hashTable);
+
+    cout << "Indexes initialized, rehashing old data..." << endl;
+
+    // Run through old array and rehash all data to new array
+    for (int i = 0; i < spaceUsed; i++)
+    {
+        int newIndex = hash(tempTable[i]);
+        hashTable[newIndex] = tempTable[i];
+    }
+
+    cout << "Old data rehashed, readding last item..." << endl;
+
+    addItem(addData);
+
+    cout << "Deleting tempTable..." << endl << endl;
+
+    delete[] tempTable;
 }
 
 template <class T>
 int Hashtable<T>::hash(T data)
 {
-    if (data % 10 == 0)
+    int hashAddress = 0;
+    for (int counter = 0; data[counter] != '\0'; counter++)
     {
-        data++;
+        hashAddress = data[counter] + (hashAddress << 6) + (hashAddress << 16) - hashAddress;
     }
+    hashAddress = hashAddress & hashMask;
 
-    data = data * 42 % MAX;
-
-    return data; 
+    return hashAddress; 
 }
 
 template <class T>
-void Hashtable<T>::addItem(T newValue)
+void Hashtable<T>::addItem(T newData)
 {
-    int index = hash(newValue);
+    int index = hash(newData);
+    //cout << "Hashed index: " << index << endl;
 
-    if (myTable[index] == NULL)
+    if (spaceUsed + 1 > rehashThreshold)
     {
-        myTable[index] = newValue;
+        growTable(hashTable, newData);
     }
     else
     {
-        while(myTable[index] != NULL)
+        if (hashTable[index] == "")
         {
-            if (myTable[index] < MAX)
+            hashTable[index] = newData;
+            spaceUsed++;
+            //cout << newData << " has been added to the table" << endl << endl;
+        }
+        else
+        {
+            while (hashTable[index] != "")
             {
                 index++;
+                if (index == capacity)
+                {
+                    index = 0;
+                }
             }
-            else
-            {
-                index = 0;
-            }
+            hashTable[index] = newData;
+            spaceUsed++;
         }
-        myTable[index] = newValue;
-        collisionTable[collisionIndex] = newValue;
-        collisionIndex++;
-
+        /*
+        // Doesn't handle collisions
+        else
+        {
+            //cout << "Collision occured, overwriting -> " << hashTable[index] << " with -> " << newData << endl << endl; 
+            hashTable[index] = newData;
+        }
+        */
     }
 }
 
@@ -118,23 +234,47 @@ void Hashtable<T>::deleteItem(T deleteData)
 {
     int index = hash(deleteData);
 
-    if (myTable[index] == deleteData)
+    if (hashTable[index] == deleteData)
     {
-        myTable[index] = NULL;
+        hashTable[index] = "";
         cout << "The value " << deleteData << " has been removed from the table" << endl << endl;
+        spaceUsed--;
     }
     else
     {
-        cout << "The value " << deleteData << " wasn't found in the table" << endl << endl;
+        int lookupIndex = index;
+        while (hashTable[index] != deleteData)
+        {
+            lookupIndex++;
+            if (lookupIndex == capacity)
+            {
+                lookupIndex = 0;
+            }
+            if (lookupIndex == index)
+            {
+                cout << "The value " << deleteData << " wasn't found in the table" << endl << endl;
+                break;
+            }
+        }
+        if (hashTable[lookupIndex] == deleteData)
+        {
+            hashTable[lookupIndex] = "";
+            cout << "The value " << deleteData << " has been removed from the table" << endl << endl;
+            spaceUsed--;
+        }
     }
 }
 
 template <class T>
 void Hashtable<T>::printList() // Total (worst case): O(2n)
 {
-    for (int i = 0; i < MAX; i++) // O(n)
+    for (int i = 0; i < capacity; i++) // O(n)
     {
-        cout << myTable[i] << endl; // O(n)
+        if (hashTable[i] != "")
+        {
+            cout << hashTable[i] << endl; // O(n)
+        }
     }
+    cout << endl;
 }
 
--- project07_hashtable/hashtable/testHashtable.cpp
+++ project07_hashtable/hashtable/testHashtable.cpp
@@ -9,27 +9,66 @@
 //*****************************************************************
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 
 #include "Hashtable.h"
 
 using std::cout;
 using std::endl;
+using std::ifstream;
+using std::cerr;
+using std::string;
 
 
 int main(int argc, char** argv)
 {
-    Hashtable<int> myHash;
+    Hashtable<string> myHash;
 
-    myHash.addItem(10);
-    myHash.addItem(67);
-    myHash.addItem(42);
-    // HANGS HERE myHash.addItem(36);
-    //myHash.addItem(13);
-    //myHash.addItem(77);
-    //myHash.addItem(44);
-    //myHash.addItem(41);
-    //myHash.addItem(89);
-    //myHash.addItem(127);
+    myHash.addItem("banana");
+    myHash.addItem("fork");
+    myHash.addItem("chicken");
+    myHash.addItem("burger");
+    myHash.addItem("lettuce");
+    myHash.addItem("knife");
+    myHash.addItem("apple");
+    myHash.addItem("fish");
+    myHash.addItem("salt");
+    myHash.addItem("water");
+    myHash.addItem("waffle");
+    myHash.addItem("potato");
+
+    myHash.printList();
+
+    myHash.deleteItem("apple");
+    myHash.deleteItem("salt");
+    myHash.deleteItem("banana");
+    myHash.deleteItem("burger");
+
+    myHash.printList();
+
+    cout << "Adding data from external source..." << endl << endl;
+
+    // Using input stream to add a massive amount of entries to the table
+    // First we declare an input file stream variable
+    ifstream inFile;
+    // Then we open the stream
+    inFile.open("table_entries.txt");
+    // Check is file was opened successfully
+    if (!inFile)
+    {
+        cerr << "Unable to open file table_entries.txt" << endl << endl;
+        exit(1); // Stop program
+    }
+
+    // Read the stream
+    string x;
+    while (inFile >> x)
+    {
+        myHash.addItem(x);
+    }
+
+    //close the stream
+    inFile.close();
 
     myHash.printList();
 
